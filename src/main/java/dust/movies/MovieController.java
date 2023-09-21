9be3487bb308428e65aa5d44c9801a24bee9bc4d
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-09-21 13:40:26 +0200

    Basic get and post api endpoints

--- src/main/java/dust/movies/MovieController.java
+++ src/main/java/dust/movies/MovieController.java
@@ -3,15 +3,29 @@ package dust.movies;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
     @GetMapping
-    public ResponseEntity<String> allMovies()
-    {
-        return new ResponseEntity<String>("Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getMovieList() {
+        return new ResponseEntity<List<Movie>>(movieService.movieList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.movie(imdbId), HttpStatus.OK);
     }
 }
--- src/main/java/dust/movies/MovieService.java
+++ src/main/java/dust/movies/MovieService.java
@@ -2,6 +2,22 @@ package dust.movies;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> movieList() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> movie(String imdbId){
+        return movieRepository.getMovieByImdbId(imdbId);
+    }
 }
